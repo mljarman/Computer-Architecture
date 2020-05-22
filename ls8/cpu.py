a3d0c7da9a7101bd2d3d5156a21ef3150bfbb537
mljarman <maggiejarman2@gmail.com>
2020-05-22 10:36:40 -0600

    day 5 MVP and sprint

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -10,6 +10,7 @@ class CPU:
         self.reg = [0] * 8
         self.ram = [0] * 256
         self.pc = 0
+        self.fl = {'E': 0, 'L': 0, 'G':0}
 
         # self.branchtable = {}
         # self.branchtable[HLT] = self.hlt
@@ -51,11 +52,26 @@ class CPU:
         """ALU operations."""
 
         if op == "ADD":
+            # add the values of two regiesters and
+            # store in reg_a:
             self.reg[reg_a] += self.reg[reg_b]
 
         elif op == "MUL":
+            # multiply the values of two registers and
+            # store in reg_a:
             self.reg[reg_a] *= self.reg[reg_b]
-        #elif op == "SUB": etc
+
+        elif op == "CMP":
+            # compare the values in two registers:
+            # if they are equal:
+            if self.reg[reg_a] == self.reg[reg_b]:
+                self.fl['E'] = 1
+            # if reg_a is less than reg_b:
+            if self.reg[reg_a] < self.reg[reg_b]:
+                self.fl['L'] = 1
+            # if reg_a is greater than reg_b:
+            if self.reg[reg_a] > self.reg[reg_b]:
+                self.fl['G'] = 1
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -89,60 +105,34 @@ class CPU:
         SP = 7
         self.reg[SP] = 0xf4
 
-        HLT = 0b00000001
-        LDI = 0b10000010
-        PRN = 0b01000111
-        MUL = 0b10100010
-        PUSH = 0b01000101
-        POP = 0b01000110
-        CALL = 0b01010000
-        RET = 0b00010001
         ADD = 0b10100000
+        CALL = 0b01010000
+        CMP = 0b10100111
+        HLT = 0b00000001
+        JEQ =  0b01010101
+        JMP = 0b01010100
+        JNE = 0b01010110
+        LDI = 0b10000010
+        MUL = 0b10100010
+        POP = 0b01000110
+        PRN = 0b01000111
+        PUSH = 0b01000101
+        RET = 0b00010001
+        #TRY = 0b00000000
 
         halted = False
+
 
         while not halted:
             instruction = self.ram_read(IR)
             operand_a = self.ram_read(IR + 1)
             operand_b = self.ram_read(IR + 2)
 
-
-            if instruction == LDI:
-                reg_num = operand_a
-                value = operand_b
-                self.reg[reg_num] = value
-                IR += 3
-            elif instruction == PRN:
-                reg_num = self.reg[operand_a]
-                print(reg_num)
-                IR += 2
-            elif instruction == MUL:
-                self.alu('MUL', operand_a, operand_b)
-                IR += 3
-            elif instruction == ADD:
+            if instruction == ADD:
+                # call ALU to add 2 registers and store in reg_a
                 self.alu('ADD', operand_a, operand_b)
+                # 3 bit operation
                 IR += 3
-            elif instruction == PUSH:
-                # decrement SP:
-                self.reg[SP] -= 1
-                # get register #:
-                reg_num = operand_a
-                # get value out of register:
-                value = self.reg[reg_num]
-                # store value in memory at SP:
-                top_of_stack_addr = self.reg[SP]
-                self.ram[top_of_stack_addr] = value
-                IR += 2
-            elif instruction == POP:
-                reg_num = operand_a
-                #Copy the value from the address pointed to by SP to the given register.
-                top_of_stack_addr = self.reg[SP]
-                value = self.ram[top_of_stack_addr]
-                # store the value to the given register:
-                self.reg[reg_num] = value
-                #Increment SP.
-                self.reg[SP] += 1
-                IR += 2
 
             elif instruction == CALL:
                 return_addr = IR + 2
@@ -157,15 +147,96 @@ class CPU:
                 # the subroutine_addr
                 IR = subroutine_addr
 
-            elif instruction == RET:
-                top_of_stack_addr = self.reg[SP]
-                return_addr = self.ram[top_of_stack_addr]
-                self.reg[SP] += 1
-
-                IR = return_addr
+            elif instruction == CMP:
+                self.alu('CMP', operand_a, operand_b)
+                # 3 bit operation
+                IR += 3
 
             elif instruction == HLT:
+                # halt program
                 halted = True
+
+            elif instruction == JEQ:
+                # if equal flag is set to true (1):
+                if self.fl['E'] == 1:
+                    # jump to the address stored in the given register:
+                    reg_num = operand_a
+                    addr = self.reg[reg_num]
+                    IR = addr
+                else:
+                    IR += 2
+
+            elif instruction == JMP:
+                # jump to address stored in the given register
+                reg_num = operand_a
+                addr_value = self.reg[reg_num]
+                IR = addr_value
+
+            elif instruction == JNE:
+                # if equal flag is set to false (0), jump to the address stored
+                # in registerB
+                if self.fl['E'] == 0:
+                    reg_num = operand_a
+                    addr = self.reg[reg_num]
+                    IR = addr
+                else:
+                    IR += 2
+
+            elif instruction == LDI:
+                reg_num = operand_a
+                value = operand_b
+                self.reg[reg_num] = value
+                IR += 3
+
+
+            elif instruction == MUL:
+                # call ALU to multiply 2 registers and store in reg_a
+                self.alu('MUL', operand_a, operand_b)
+                # 3 bit operation
+                IR += 3
+
+            elif instruction == POP:
+                reg_num = operand_a
+                # copy the value from the address pointed to by SP to the given register.
+                top_of_stack_addr = self.reg[SP]
+                value = self.ram[top_of_stack_addr]
+                # store the value to the given register:
+                self.reg[reg_num] = value
+                # increment SP.
+                self.reg[SP] += 1
+                # 2 bit operation
+                IR += 2
+
+            elif instruction == PRN:
+                # print numeric value stored in given register
+                value = self.reg[operand_a]
+                print(value)
+                # 2 bit operation
+                IR += 2
+
+            elif instruction == PUSH:
+                # decrement SP:
+                self.reg[SP] -= 1
+                # get register #:
+                reg_num = operand_a
+                # get value out of register:
+                value = self.reg[reg_num]
+                # store value in memory at SP:
+                top_of_stack_addr = self.reg[SP]
+                self.ram[top_of_stack_addr] = value
+                # 2 bit operation
+                IR += 2
+
+            elif instruction == RET:
+                # return from subroutine
+                # pop the value from the top of the stack and store in IR
+                top_of_stack_addr = self.reg[SP]
+                return_addr = self.ram[top_of_stack_addr]
+                # increment SP
+                self.reg[SP] += 1
+                # set IR to return_address
+                IR = return_addr
+
             else:
                 print(f'unknown instruction {instruction} at address {IR}')
                 sys.exit(1)
